71d91d75513f44c89a38f0a471cc376a0f018fef
tycrek <josh.moore@jmoore.dev>
2020-05-21 17:02:11 -0600

    Trim text in build task

--- _build.js
+++ _build.js
@@ -8,12 +8,11 @@ const YAML = require('yaml');
 function __main__() {
     let lines = `| Name | Eyes | Description |${os.EOL}| ---- | ---- | ----------- |${os.EOL}`;
 
-
     let dgData = YAML.parse(fs.readFileSync(path.join(__dirname, 'degoogle.yml')).toString());
     dgData['web based products'].forEach(item => {
         let name = `[${item.name}](${item.url})`;
         let eyes = item.eyes ? `**${item.eyes}-eyes**` : '';
-        let text = item.text;
+        let text = item.text.trim();
 
         let entry = `| ${name} | ${eyes} | ${text} |`;
         
